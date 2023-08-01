8153d3055ee5bf032eed9e65e10b61552f1ea088
animalplay <juandavidgonzalezurrego@gmail.com>
2023-07-31 21:29:04 -0500

    finish playground with DOM JS

--- toppings-pizza/app.js
+++ toppings-pizza/app.js
@@ -13,4 +13,30 @@ console.log(fondosNaranjas);
 
 //Estilos CSS
 const primerTopping = document.querySelector('.topping');
-primerTopping.style.backgroundColor = 'blue';
+primerTopping.style.backgroundColor = 'blue';''
+primerTopping.classList.add('demostracion');
+//Obtener clases
+console.log(primerTopping.classList);
+
+primerTopping.classList.remove('demostracion');
+//Obtener clases
+console.log(primerTopping.classList);
+
+//Agregar elementos
+const listaDeToppings = document.getElementById('toppings-list');
+const toppingNuevo = document.createElement('li');
+toppingNuevo.classList.add('topping', 'fondo-marron');
+toppingNuevo.innerText = 'Queso Extra'
+
+listaDeToppings.append(toppingNuevo);
+
+toppingNuevo.remove();
+
+//Jugando con eventos :D
+function mostrarClick(e){
+    console.log(e.target.innerText);
+}
+
+for(const topping of toppings){
+    topping.addEventListener('click', mostrarClick);
+}
